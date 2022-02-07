23b6d84cf5a50a8dfe1d6747ac0f2be09ed85ab5
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-02-07 18:03:43 -0300

    chore: extract genres and clients to separate JSON files

--- src/lib/kataGenerator.js
+++ src/lib/kataGenerator.js
@@ -1,57 +1,10 @@
 import uniqueRandomArray from "unique-random-array";
 
-const GENRES = [
-  "Open-world game",
-  "Side-scrolling shooter",
-  "Dating simulator",
-  "Tower defense game",
-  "Party game",
-  "Fighting game",
-  "Rhythm game",
-  "Survival horror game",
-  "Battle royale game",
-  "Sandbox game",
-  "Stealth game",
-  "JRPG",
-  "Soulslike",
-  "Endless runner game",
-  "Tactical RPG",
-  "Grand strategy game",
-  "Visual novel",
-  "Walking simulator",
-  "Cooking game",
-];
+import genres from "../genres.json";
+import clients from "../clients.json";
 
-const CLIENTS = [
-  "All Elite Wrestling",
-  "Doctors Without Borders",
-  "AGDQ",
-  "The Weeknd",
-  "Studio Ghibli",
-  "Evangelion",
-  "Shingeki no Kyojin",
-  "the Opus Dei",
-  "the Church of Scientology",
-  "The Pokémon Company",
-  "Christopher Nolan",
-  "The Office",
-  "Gordon Ramsay",
-  "Hannibal Lecter",
-  "Tom Nook",
-  "the Jurassic Park franchise",
-  "the Minority Report movie",
-  "the Kill Bill franchise",
-  "The Hobbit",
-  "Filthy Frank",
-  "the Nvidia brand",
-  "1984",
-  // "Perro Matapacos",
-  // "31 Minutos",
-  // "Video Loco",
-  // "El Discípulo del Chef",
-  // "Bomberos de Chile",
-  // "the Transantiago",
-];
+const GENRES = genres.genres;
+const CLIENTS = clients.clients;
 
 Object.freeze(GENRES);
 Object.freeze(CLIENTS);
